53ecad5ca9688076636b3eee29fd44116eb141ff
Seung Yun LEE <soul3914@naver.com>
2023-01-11 02:52:31 +0900

    JDBC

--- src/JDBC/JDBCTest.java
+++ src/JDBC/JDBCTest.java
@@ -3,7 +3,6 @@ package JDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JDBCTest {
@@ -31,30 +30,111 @@ public class JDBCTest {
 			
 			
 			// db에 데이터 삽입 // 
-			String sql = "INSERT INTO java VALUES(null,?,?)";
+//			String sql = "INSERT INTO java VALUES(null,?,?)";
+//			
+//			PreparedStatement statement = conn.prepareStatement(sql);
+//			statement.setString(1, "김개똥");
+//			statement.setString(2, "1234567");
+//			
+//			int rows = statement.executeUpdate();
+//			System.out.println(rows + "개의 데이터가 삽입되었습니다");
+//			
+//			
+//			
+//			// db에 데이터 삽입하고 삽입한 데이터의 특정 칼럼 가져오기 // 
+//			statement = conn.prepareStatement(sql,new String[] {"id"});
+//			statement.setString(1, "가져와");
+//			statement.setString(2, "비밀번호");
+//			
+//			rows = statement.executeUpdate();
+//			ResultSet rs = statement.getGeneratedKeys();
+//			if(rs.next()) {
+//				int i = rs.getInt(1);
+//				System.out.println("저장된 번호 : " + i);
+//			}
+//			
 			
-			PreparedStatement statement = conn.prepareStatement(sql);
-			statement.setString(1, "김개똥");
-			statement.setString(2, "1234567");
+//			// 데이터 수정하기 // 
+//			String sql = "UPDATE java "
+//					+ "SET name = ?, "
+//					+ "password = ? "
+//					+ "WHERE id = ?";
+//			
+//			PreparedStatement statement = conn.prepareStatement(sql);
+//			statement.setString(1, "김규동");
+//			statement.setString(2, "비밀번호규동이");
+//			statement.setInt(3, 1);
+//			int rows= statement.executeUpdate();
 			
-			int rows = statement.executeUpdate();
-			System.out.println(rows + "개의 데이터가 삽입되었습니다");
+			
+//			//데이터 삭제하기 //
+//			String sql = "DELETE FROM java WHERE id > 5";
+//			
+//			PreparedStatement statement = conn.prepareStatement(sql);
+//			
+//			int rows = statement.executeUpdate();
+//			System.out.println(rows +  "개의 행 삭제 됨");
+			
+//			String sql = "SELECT name FROM java WHERE id < 5";
+//			PreparedStatement statement = conn.prepareStatement(sql);
+//			
+//			ResultSet rs = statement.executeQuery();
+//			
+//			while(rs.next())
+//			{
+//				System.out.println(rs.getString(1)); // 매개값 1은 첫번째 칼럼을 의미 
+//			}
+//			
+//			rs.close();
+			
+//			// 프로시저 호출 // 
+//			Scanner scan = new Scanner(System.in);
+//			System.out.print("이름 입력 : " );
+//			
+//			String user_id = scan.nextLine();
+//			
+//			System.out.print("패스워드 입력 : " );
+//			String user_pass = scan.nextLine();
+//			
+//			String sql = "{call USER_CREATE(?,?)}";
+//			CallableStatement cs = (CallableStatement) conn.prepareCall(sql);
+//			cs.setString(1, user_id);
+//			cs.setString(2, user_pass);
+//			
+//			
+//			cs.execute();
+//			System.out.println("프로시저 호출 완료");
+//			cs.close();
+			
+			
+			// 트랜잭션 처리 ///
+			
+			String sql = "UPDATE java SET id = id+1000";
+			String sql2 = "UPDATE java SET password = 0000";
 			
 			
 			
-			// db에 데이터 삽입하고 삽입한 데이터의 특정 칼럼 가져오기 // 
-			statement = conn.prepareStatement(sql,new String[] {"id"});
-			statement.setString(1, "가져와");
-			statement.setString(2, "비밀번호");
-			
-			rows = statement.executeUpdate();
-			ResultSet rs = statement.getGeneratedKeys();
-			if(rs.next()) {
-				int i = rs.getInt(1);
-				System.out.println("저장된 번호 : " + i);
+			try {
+				conn.setAutoCommit(false);
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.executeUpdate();
+				
+				statement = conn.prepareStatement(sql2);
+				statement.executeUpdate();
+				
+				System.out.println("커밋됨");
+				conn.commit();
+			}catch(Exception e)
+			{
+				
+				conn.rollback();
+			}
+			finally {
+				conn.setAutoCommit(true);
 			}
 			
-			statement.close();
+			
+			
 			
 			// DB 연결 끊기 
 			conn.close();
